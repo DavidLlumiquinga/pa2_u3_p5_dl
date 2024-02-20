98e701723486b9159a1658d78b0403da34d3140b
unknown <mdllumiquingat@uce.edu.ec>
2024-02-19 21:03:37 -0500

    Taller35

--- src/main/java/pa2_u3_p5_dl/Pa2U3P5DlApplication.java
+++ src/main/java/pa2_u3_p5_dl/Pa2U3P5DlApplication.java
@@ -1,18 +1,22 @@
 package pa2_u3_p5_dl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import pa2_u3_p5_dl.ventas.service.IFacturaService;
+import pa2_u3_p5_dl.ventas.repository.modelo.Cliente;
+import pa2_u3_p5_dl.ventas.service.IClienteService;
 
 @SpringBootApplication
 public class Pa2U3P5DlApplication implements CommandLineRunner {
 
 	@Autowired
-	private IFacturaService iFacturaService;
-	
+	private IClienteService iClienteService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U3P5DlApplication.class, args);
 	}
@@ -20,9 +24,51 @@ public class Pa2U3P5DlApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-
-		//this.iFacturaService.pruebaSupports();
-		//this.iFacturaService.pruebaNever();
+		/*
+		 * System.out.println("Nombre hilo: "+Thread.currentThread().getName()); //Se
+		 * ejecuta en un hilo long tiempoInicial=System.currentTimeMillis();
+		 * 
+		 * for(int i=1; i<=100; i++) { Cliente cliente=new Cliente();
+		 * cliente.setNombre("CN"+i); cliente.setApellido("CA"+i);
+		 * this.iClienteService.guardar(cliente); }
+		 * 
+		 * long tiempoFinal=System.currentTimeMillis();
+		 * 
+		 * long tiempoTranscurrido=(tiempoFinal - tiempoInicial)/1000;
+		 * System.out.println("Tiempo transcurido en seg: "+tiempoTranscurrido);
+		 */
+		//Programacion en paralelo (Multihilo- MultiTherad)
+//		System.out.println("Nombre hilo: "+Thread.currentThread().getName()); //Se
+//		long tiempoInicial = System.currentTimeMillis();
+//		List<Cliente> listaCliente = new ArrayList<>();
+//		for (int i = 1; i <= 100; i++) {
+//			Cliente cliente = new Cliente();
+//			cliente.setNombre("CN" + i);
+//			cliente.setApellido("CA" + i);
+//			listaCliente.add(cliente);
+//		}
+//		listaCliente.stream().forEach(cliente -> this.iClienteService.guardar(cliente));
+//		long tiempoFinal = System.currentTimeMillis();
+//
+//		long tiempoTranscurrido = (tiempoFinal - tiempoInicial) / 1000;
+//		System.out.println("Tiempo transcurido en seg: " + tiempoTranscurrido);
+	//Tiempo transcurrido == 6
 		
+		System.out.println("Nombre hilo: "+Thread.currentThread().getName()); //Se
+		long tiempoInicial = System.currentTimeMillis();
+		List<Cliente> listaCliente = new ArrayList<>();
+		for (int i = 1; i <= 100; i++) {
+			Cliente cliente = new Cliente();
+			cliente.setNombre("CN" + i);
+			cliente.setApellido("CA" + i);
+			listaCliente.add(cliente);
+		}
+		listaCliente.parallelStream().forEach(cliente -> this.iClienteService.guardar(cliente));
+		long tiempoFinal = System.currentTimeMillis();
+
+		long tiempoTranscurrido = (tiempoFinal - tiempoInicial) / 1000;
+		System.out.println("Tiempo transcurido en seg: " + tiempoTranscurrido);
+		
+		//Tiempo transcurrido == 1
 	}
 }
--- src/main/java/pa2_u3_p5_dl/ventas/service/ClienteServiceImpl.java
+++ src/main/java/pa2_u3_p5_dl/ventas/service/ClienteServiceImpl.java
@@ -1,5 +1,7 @@
 package pa2_u3_p5_dl.ventas.service;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
@@ -19,15 +21,19 @@ public class ClienteServiceImpl implements IClienteService {
 	@Override
 	@Transactional(value = TxType.REQUIRES_NEW)
 	public void guardar(Cliente cliente) {
-		// TODO Auto-generated method stub
-		try {
-			this.iClienteRepository.insertar(cliente);
-		} catch (RuntimeException e) {
-			System.out.println(e.getClass());
-
-		}
-	}
-
+        try {
+            this.iClienteRepository.insertar(cliente);
+            //simulando demora por diferentes consultas
+            TimeUnit.SECONDS.sleep(1);
+        } catch (RuntimeException e) {
+            // TODO: handle exception
+            System.out.println(e.getClass());
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }//commit
 	@Override
 	@Transactional(value = TxType.SUPPORTS)
 	public void pruebaSupports() {
